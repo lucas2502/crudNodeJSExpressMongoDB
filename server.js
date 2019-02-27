f55b39b39979cb70ab69986935cf6e8329fe3ad2
Lucas Gonçalves de França <lucas@grupoelosaude.com.br>
2019-02-27 17:55:42 -0300

    Config connection mongoDB(error)

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 
+const uri = "mongodb://admin:admin@server1:27017,server2:27017,server3:27017/crud-nodejs?ssl=true&replicaSet=example-shard-0&authSource=admin";
 const app = express();
+
+
 
 app.use(bodyParser.urlencoded({ extended:true }));
 
@@ -12,9 +16,19 @@ app.get('/', (req, res) => {
     res.render('index.ejs');
 })
 app.post('/show', (req, res) => {
-    console.log(req.body);
+    db.collection('data').save(req.body, (err, result) =>{
+        if(err) return console.log(err);
+    })
+
+    console.log('Saved in BD');
+    res.redirect('/');
 })
 
-app.listen(3000, () => {
-    console.log('server running on port 3000');
+MongoClient.connect(uri, (err, client) =>{
+    if (err) return console.log(err);
+    db = client.db('crud-nodejs')
+
+    app.listen(3000, () =>{
+        console.log('server running on port 3000');
+    })
 })
