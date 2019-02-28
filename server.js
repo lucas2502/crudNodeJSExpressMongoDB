e55b2abe4357de09ce299d56ad74a1eab5f8d9d5
Lucas Gonçalves de França <lucas@grupoelosaude.com.br>
2019-02-28 10:52:54 -0300

    Create GET

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-
-const uri = "mongodb+srv://admin:admin@cluster0-rx5e2.mongodb.net/crud-nodejs?retryWrites=true";
 const app = express();
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:admin@cluster0-rx5e2.mongodb.net/crud-nodejs?retryWrites=true";
+
+MongoClient.connect(uri, (err, client) =>{
+    if (err) return console.log(err);
+    db = client.db('crud-nodejs');
+
+    app.listen(3000, () => {
+        console.log('server running on port 3000');
+    })
+})
 
 
 app.use(bodyParser.urlencoded({ extended:true }));
@@ -15,20 +23,24 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('index.ejs');
 })
-app.post('/show', (req, res) => {
-    db.collection('data').save(req.body, (err, result) =>{
-        if(err) return console.log(err);
-    })
 
-    console.log('Saved in BD');
-    res.redirect('/');
+app.get('/', (req, res) => {
+    const cursor = db.collection('data').find();
 })
 
-MongoClient.connect(uri, (err, client) =>{
-    if (err) return console.log(err);
-    db = client.db('crud-nodejs')
+//insert in bd
+app.get('/show', (req, res) => {
+    db.collection('data').find().toArray((err, results) => {
+        if(err) return console.log(err);
+        res.render('show.ejs', { data: results });
+    })
+})
 
-    app.listen(3000, () =>{
-        console.log('server running on port 3000');
+app.post('/show', (req, res) => {
+    db.collection('data').save(req.body, (err, result) => {
+        if(err) return console.log(err);
+
+        console.log('Saved in BD');
+        res.redirect('/show');
     })
 })
