91dd44c81de8045ec0d7930f1fe2126f8b2aa043
Lucas Gonçalves de França <lucas@grupoelosaude.com.br>
2019-02-28 18:00:52 -0300

    Create UPDATE

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://admin:admin@cluster0-rx5e2.mongodb.net/crud-nodejs?retryWrites=true";
 
-MongoClient.connect(uri, (err, client) =>{
+MongoClient.connect(uri, (err, client) => {
     if (err) return console.log(err);
     db = client.db('crud-nodejs');
 
@@ -42,5 +42,31 @@ app.post('/show', (req, res) => {
 
         console.log('Saved in BD');
         res.redirect('/show');
+    })
+})
+
+app.route('/edit/:id')
+.get((req, res) => {
+    var id = req.params.id
+    
+
+    db.collection('data').find(ObjectId(id)).toArray((err, result) => {
+        if(err) return res.send(err)
+        res.render('edit.ejs', { data: result })
+    })
+})
+.post((req, res) => {
+    var id = req.params.id;
+    var name = req.body.name;
+    var surname = req.body.surname;
+    db.collection('data').updateOne({_id: ObjectId(id)}, {
+        $set: {
+            name,
+            surname
+        }
+    }, (err, result) => {
+        if(err) return res.send(err)
+        res.redirect('/show');
+        console.log('Atualizado no banco de Dados');
     })
 })
